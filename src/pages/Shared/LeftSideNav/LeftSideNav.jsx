cc9aa84340179ba5c76fcba6d024818a7bd978e3
IEEEMOSTAFA <skmostafa8888@gmail.com>
2025-02-12 08:12:43 +0600

    useEffect() added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
     return (
-        <div>
-            <h2 className="text-3xl">Right side Nav</h2>
+        <div className='space-y-10'>
+            <h2 className="text-3xl">All Categories : {categories.length}</h2>
+
+            {
+                categories.map(category => 
+                <Link 
+                className='block ml-4 text-xl font-semibold px-4'
+                key={category.id}
+                to={`/category/${category.id}`}
+                
+                >{category.name}</Link>)
+            }
         </div>
     );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -4,7 +4,9 @@ import { FaGithub } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
-
+import qZone1 from '../../../assets/swimming.png';
+import qZone2 from '../../../assets/class.png';
+import qZone3 from '../../../assets/playground.png';
 
 
 
@@ -28,11 +30,28 @@ const RightSideNav = () => {
             </div>
             {/* 2nd */}
 
+            <div className="p-4 mb-4">
+                <h2 className="text-3xl mb-3">Find Us On</h2>
+                <a className="p-4 flex text-lg items-center border rounded-lg" href="">
+                    <FaFacebookF className="mr-3" /> <span>Facebook</span>
+                </a>
+                <a className="p-4 flex text-lg items-center border rounded-lg -mt-px" href="">
+                    <FaXTwitter className="mr-3" /><span>Twitter</span>
+                </a>
+                <a className="p-4 flex text-lg items-center border rounded-lg -mt-px" href="">
+                    <FaInstagram className="mr-3" /> <span>Instagram</span>
+                </a>
+            </div>
+
+
+            {/* Queue Zone */}
+
             <div className='p-4 space-y-3 mb-6'>
-                <h2 className="text-3xl ">Find Us On</h2>
-                <a className='p-4 flex text-lg items-center border rounded-lg' href=""><FaFacebookF className='mr-3' /> <span>Facebook</span></a>
-                <a className='p-4 flex text-lg items-center border rounded-lg' href=""><FaXTwitter className='mr-3' /><span>Twitter</span></a>
-                <a className='p-4 flex text-lg items-center border rounded-lg' href=""><FaInstagram className='mr-3' /> <span>Instagram</span></a>
+                <h2 className="text-3xl "></h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+
             </div>
 
 
